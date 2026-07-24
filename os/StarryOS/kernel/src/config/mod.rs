a74bed4c0fc3034b319896cd2def4cda5b9a55fc
lxg <1600577905@qq.com>
2026-07-24 16:13:35 +0800

    删除x86、riscv64架构代码

--- os/StarryOS/kernel/src/config/mod.rs
+++ os/StarryOS/kernel/src/config/mod.rs
@@ -1,19 +1,7 @@
 //! Architecture-specific configurations.
 
 cfg_if::cfg_if! {
-    if #[cfg(target_arch = "riscv64")] {
-    #[rustfmt::skip]
-        mod riscv64;
-        pub use riscv64::*;
-    } else if #[cfg(target_arch = "loongarch64")] {
-        #[rustfmt::skip]
-        mod loongarch64;
-        pub use loongarch64::*;
-    } else if #[cfg(target_arch = "x86_64")] {
-        #[rustfmt::skip]
-        mod x86_64;
-        pub use x86_64::*;
-    } else if #[cfg(target_arch = "aarch64")] {
+    if #[cfg(target_arch = "aarch64")] {
         #[rustfmt::skip]
         mod aarch64;
         pub use aarch64::*;
